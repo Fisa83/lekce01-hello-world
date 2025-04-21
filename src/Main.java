c8ec19abf1824ba7394190746ea05f5cddb6a6d7
Fiser Tomas <fiser.tomas@feg.eu>
2025-04-21 14:34:16 +0200

    Přidaní metody pro výpis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Main.bagr();
+    }
+
+    /**
+     * Tohle je metoda co vypíše texty.
+     */
+    public static void bagr() {
+        System.out.println("Bagr!");
+        System.out.print("Test"); // nečum
+        System.out.print("nejkuliťoulinkatější");
     }
 }
